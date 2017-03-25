48d78e7f23b60851996a9800f83a37c684d894a9
Rhilip <rhilipruan@gmail.com>
2017-03-25 13:08:05 +0800

    v20170325 完善下载函数，针对昨天版本中可能出现的问题进行修正

--- model/download.py
+++ model/download.py
@@ -16,7 +16,7 @@ import requests
 
 from threading import Thread
 
-# TODO Use Thread to Download multiple files at the same time
+
 class DownloadQueue(Thread):
     from queue import Queue
     queue = Queue()
@@ -42,50 +42,55 @@ def download_file(session: requests.Session(), url: str, file: str, resume=True,
     else:
         resume_len = 0
         file_mode = 'wb'
-    #
-    attempts_count = 0
-    while attempts_count < retry:
-        response = session.get(url, stream=True)
-        # 检查响应流状况
-        if response.status_code != 200 and response.status_code != 206:
-            if response.reason:
-                error_msg = response.reason + ' ' + str(response.status_code)
-            else:
-                error_msg = 'HTTP Error ' + str(response.status_code)
 
-            if attempts_count + 1 < retry:
-                wait_interval = 2 ** (attempts_count + 1)  # 指数退让
-                msg = 'Error downloading, will retry in {0} seconds ...'
-                print(msg.format(wait_interval))
-                time.sleep(wait_interval)
-                attempts_count += 1
-                continue
-            else:
-                raise ConnectionError(error_msg)
+    try:
+        attempts_count = 0
+        while attempts_count < retry:
+            pre_response = session.get(url, stream=True)
+            # 已下载完成检查
+            total_length = int(pre_response.headers['content-length'])
+            if resume_len != 0:
+                # 将文件总长度并与本地文件对比
+                if total_length is None or resume_len == total_length:
+                    print('{0} is Already downloaded.'.format(name))
+                    break
 
-        # 获取文件总长度
-        total_length = int(response.headers['content-length'])
+            # 构造下载请求
+            session.headers['Range'] = 'bytes=%d-' % resume_len
+            response = session.get(url, stream=True)
 
-        if resume_len != 0:
-            if total_length is None or resume_len == total_length:
-                print('{0} is Already downloaded.'.format(name))
+            # 响应流异常处理
+            if response.status_code != 200 and response.status_code != 206:
+                # 构建异常报文
+                error_msg = "HTTP Error,Code:{0} ".format(response.status_code)
+                if response.reason:
+                    error_msg += ",Reason:\"{0}\"".format(response.reason)
+
+                # 异常类型判断
+                if response.status_code == 416:  # 检查416（客户端请求字节范围无法满足） -> 禁止resume
+                    # TODO 该步的必要性还值得讨论，直接禁止resume是否过于暴力
+                    print("local file:\"{0}\" may wrong,Stop resume.".format(name))
+                    raise ValueError(error_msg)
+                if attempts_count < retry:
+                    wait_interval = min(2 ** (attempts_count + 1), 60)  # Exponential concession，Max 60s
+                    print('Error to download \"{0}\", will retry in {1} seconds ...'.format(name, wait_interval))
+                    time.sleep(wait_interval)
+                    attempts_count += 1
+                    continue
+                else:
+                    raise ConnectionError(error_msg)
+
+            # 写入工作流
+            with open(file, file_mode) as f:
+                chunk_size = 4 * (1024 * 1024)
+                widgets = [name, " ", progressbar.Bar(marker=">", left="[", right="]"), " ",
+                           progressbar.Percentage(), " ", " ", progressbar.ETA(), " ", progressbar.FileTransferSpeed()]
+                pbar = progressbar.ProgressBar(widgets=widgets, maxval=total_length).start()
+                for chunk in response.iter_content(chunk_size=chunk_size):
+                    if chunk:
+                        f.write(chunk)
+                        pbar.update(os.path.getsize(file))
+                pbar.finish()
                 break
-
-        session.headers['Range'] = 'bytes=%d-' % resume_len
-        response = session.get(url, stream=True)
-
-        # 写入工作流
-        with open(file, file_mode) as f:
-            chunk_size = 1048576
-            widgets = ["Progress: ", progressbar.Percentage(), " ",
-                       progressbar.Bar(marker=">", left="[", right="]"),
-                       " ", progressbar.ETA(), " ", progressbar.FileTransferSpeed()]
-            pbar = progressbar.ProgressBar(widgets=widgets, maxval=total_length).start()
-            length = 0
-            for chunk in response.iter_content(chunk_size=chunk_size):
-                if chunk:
-                    f.write(chunk)
-                    # f.flush()     # 文件先下载至缓存区，只有当文件全部下载完成才写入
-                length += len(chunk)
-                pbar.update(os.path.getsize(file))
-            pbar.finish()
+    except ValueError:
+        download_file(session, url, file, resume=False)
