61129b83b83b16282688984e2c25a87dd534aa64
Rhilip <rhilipruan@gmail.com>
2017-04-04 12:47:49 +0800

    Cookies Everywhere~

--- model/login.py
+++ model/login.py
@@ -26,18 +26,14 @@ def login_session(site="DEFAULT", conf=None) -> requests.session():
         'Connection': 'keep-alive',
     })
 
-    # 各个站点的方法
-    if site == "icourse163":
-        # 返回整理好的cookies
-        if conf.login_method == "Cookies":
-            cookies = cookies_raw2jar(conf.cookies)
-            login_session.cookies.update(cookies)
-        elif conf.login_method == "Account":
+    if conf.login_method == "Cookies":
+        cookies = cookies_raw2jar(conf.cookies)
+        login_session.cookies.update(cookies)
+
+    elif conf.login_method == "Account":
+        if site == "icourse163":
             raise IndexError("Not allow")
-    if site == "xuetangx":
-        if conf.login_method == "Cookies":
-            raise IndexError("Not allow")
-        elif conf.login_method == "Account":
+        if site == "xuetangx":
             login_session.get("http://www.xuetangx.com/csrf_token")
             # csrftoken = session.cookies['csrftoken']
             login_session.post(url="http://www.xuetangx.com/v2/login_ajax", data={
